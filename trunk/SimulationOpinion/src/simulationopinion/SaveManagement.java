c0d363b631d7498a137a7d5312b2c99d54d47f7f
coste.alex84@gmail.com <coste.alex84@gmail.com@45e18105-1520-29d8-b4dc-c7867f79a8a9>
2010-10-21 13:43:52 +0000

    Update4SaveManagement

--- trunk/SimulationOpinion/src/simulationopinion/SaveManagement.java
+++ trunk/SimulationOpinion/src/simulationopinion/SaveManagement.java
@@ -7,14 +7,10 @@ package simulationopinion;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 
