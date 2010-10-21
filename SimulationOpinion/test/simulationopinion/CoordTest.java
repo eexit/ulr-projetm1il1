606514e6ece8d92ef645273489853ac3a7cd4bce
teddieb26 <teddieb26@45e18105-1520-29d8-b4dc-c7867f79a8a9>
2010-10-21 08:06:29 +0000

    Integration de logManagement

--- SimulationOpinion/test/simulationopinion/CoordTest.java
+++ SimulationOpinion/test/simulationopinion/CoordTest.java
@@ -1,55 +1,58 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package simulationopinion;
 
 import junit.framework.TestCase;
+import org.junit.Test;
+import static org.junit.Assert.*;
 
 /**
+ *
  * @author claatik
  */
-public class CoordTest extends TestCase {
-
+public class CoordTest extends TestCase
+{
     private static Coord instance;
 
-    @Override
+    /**
+     * The methode will be called before every test methode
+     */
     public void setUp() {
         instance = new Coord();
-    }
-
-    /**
-     * Test of x method, of class Coord.
-     */
-    public void testX() {
         assertEquals(0, instance.x());
+        assertEquals(0, instance.y());
+        //assertTrue(instance.x() instanceof Integer);
+    }
+
+     /**
+     * Test of x and setX methods, of class Coord.
+     */
+    @Test
+    public void testGetAndSetX() {
+        instance.setX(3);
+        assertEquals(3, instance.x());
     }
 
     /**
-     * Test of y method, of class Coord.
+     * Test of y and setY methods, of class Coord.
      */
-    public void testY() {
-        assertEquals(0, instance.y());
+    @Test
+    public void testGetAndSetY() {
+        instance.setY(2);
+        assertEquals(2, instance.y());
     }
 
     /**
      * Test of setCoord method, of class Coord.
      */
+    @Test
     public void testSetCoord() {
         instance.setCoord(new Coord(5, 9));
         assertEquals(5, instance.x());
         assertEquals(9, instance.y());
     }
 
-    /**
-     * Test of setX method, of class Coord.
-     */
-    public void testSetX() {
-        instance.setX(3);
-        assertEquals(3, instance.x());
-    }
-
-    /**
-     * Test of setY method, of class Coord.
-     */
-    public void testSetY() {
-        instance.setY(2);
-        assertEquals(2, instance.y());
-    }
+   
 }
